5b4be970dcee6bcd5ac45c31720eb7fa10ad5f05
ladin22 <ladin22@naver.com>
2022-08-12 10:32:59 +0900

    [Crawler] modify save time

--- starbucks_crawler.py
+++ starbucks_crawler.py
@@ -11,10 +11,11 @@ y = -32+0.01
 
 data = []
 
+columns = ["Store Name","Street Address","Store Number","Country","Ownership Type","City","Postcode","phoneNumber","Longitude","Latitude"]
+
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 
 while x < 147:
-    y = -32+0.01
     while y < 54:
         url = "https://www.starbucks.com/store-locator?map="+str(y)+","+str(x)+",10z"
         driver.get(url)
@@ -30,24 +31,35 @@ while x < 147:
             address = str(i['address']['streetAddressLine1']) +" "+ str(i['address']['streetAddressLine2']) +" "+ str(i['address']['streetAddressLine3'])
             storenumber = i['storeNumber']
             countryCode = i['address']['countryCode']
-            data.append([name,address,storenumber,countryCode])
+            ownershiptype = i['ownershipTypeCode']
+            city = i['address']['city']
+            postcode = i['address']['postalCode']
+            phoneNumber = i['phoneNumber']
+            longitude = i['coordinates']['longitude']
+            latitude = i['coordinates']['latitude']
+            data.append([name,address,storenumber,countryCode,ownershiptype,city,postcode,phoneNumber,longitude,latitude])
         print("-------------------------------------------------------------------")
         print(str(y) + "," + str(x))
         print(len(data))
         y += 1
+
+        df = pd.DataFrame(data,columns= columns)
+
+        df.to_csv("starbucks_store_info.csv")
+
+        # df.drop_duplicates(['매장번호'],keep = 'first',inplace=True)
+
+        # writer = pd.ExcelWriter('starbucks_store_info.xlsx',engine="xlsxwriter")
+
+        # df.to_excel(writer, sheet_name='starbucks')
+
+        # writer.save()
     x += 1
+    y = -32+0.01
 
 
 
 
-columns = ["매장명","주소","매장번호","국가"]
-df = pd.DataFrame(data,columns= columns)
-writer = pd.ExcelWriter('starbucks_store_info.xlsx',engine="xlsxwriter")
-
-
-df.to_excel(writer, sheet_name='starbucks')
-
-writer.save()
 
 
 
